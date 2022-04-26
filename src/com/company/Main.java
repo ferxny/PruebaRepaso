a4e408576a272cc64b3a56ac4f13b0f785b106c4
ferxny <104160378+ferxny@users.noreply.github.com>
2022-04-26 15:27:05 +0200

    Segundo commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Aqui se muestra un texto");
+        System.out.println("Esto se ha insertado desde GitHub");
     }
 }
